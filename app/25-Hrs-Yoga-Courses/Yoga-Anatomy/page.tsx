81f30d900592c336fed369472c5b27a5312bd881
chaithanyavdeveloper <chaithanyalakshmig7@gmail.com>
2025-05-03 12:20:01 +0530

    '03-05'

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
@@ -1,13 +1,13 @@
 // app/25-Hrs-Yoga-Courses/page.tsx
-import Hero from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/Hero";
-import CourseInfo from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseInfo";
-import Schedule from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/Schedule";
-import Pricing from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/Pricing";
-import RegistrationCTA from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/RegistrationCTA";
-import Benefits from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/Benefits";
-import Teachers from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/Teachers";
-import Testimonials from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/Testimonials";
-import FAQ from "@/components/25-Hrs-Yoga-Courses/Yoga-Anatomy/FAQ";
+import Hero from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Hero";
+import CourseInfo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseInfo";
+import Schedule from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Schedule";
+import Pricing from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Pricing";
+import RegistrationCTA from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/RegistrationCTA";
+import Benefits from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Benefits";
+import Teachers from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Teachers";
+import Testimonials from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Testimonials";
+import FAQ from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/FAQ";
 
 export default function Page() {
   return (

5f25367ee5dd139f1f8ea71acaa5002a6a510204
chaithanyavdeveloper <chaithanyalakshmig7@gmail.com>
2025-05-03 15:59:10 +0530

    03-05

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
@@ -1,13 +1,6 @@
 
 "use client";// app/25-Hrs-Yoga-Courses/page.tsx
-import Hero from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Hero";
-import CourseInfo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseInfo";
-import Schedule from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Schedule";
-import Pricing from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Pricing";
-import RegistrationCTA from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/RegistrationCTA";
-import Benefits from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Benefits";
-import Teachers from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Teachers";
-import Testimonials from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Testimonials";
+
 import YogaLandingPage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/YogaLandingPage";
 
 export default function Page() {

02be2b6a7783136b53692d92dd67baef55e88fcf
chaithanyavdeveloper <chaithanyalakshmig7@gmail.com>
2025-05-03 16:55:46 +0530

    03-05

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
@@ -1,12 +1,12 @@
 
 "use client";// app/25-Hrs-Yoga-Courses/page.tsx
 
-import YogaLandingPage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/YogaLandingPage";
+import CourseDetailPage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseDetailPage";
 
 export default function Page() {
   return (
     <div>
-      <YogaLandingPage />
+      <CourseDetailPage />
     </div>
   );
 }

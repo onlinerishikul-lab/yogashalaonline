aabadb5b894c470a1068ee159e5ac7c35f0be188
chaithanyavdeveloper <chaithanyalakshmig7@gmail.com>
2025-05-03 16:57:47 +0530

    03-05

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
@@ -1,12 +1,27 @@
 
 "use client";// app/25-Hrs-Yoga-Courses/page.tsx
-
-import CourseDetailPage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseDetailPage";
+import Hero from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Hero";
+import CourseInfo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseInfo";
+import Schedule from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Schedule";
+import Pricing from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Pricing";
+import RegistrationCTA from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/RegistrationCTA";
+import Benefits from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Benefits";
+import Teachers from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Teachers";
+import Testimonials from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Testimonials";
+import FAQ from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/FAQ";
 
 export default function Page() {
   return (
     <div>
-      <CourseDetailPage />
+      <Hero />
+      <CourseInfo />
+      <Schedule />
+      <Pricing />
+      <RegistrationCTA />
+      <Benefits />
+      <Teachers />
+      <Testimonials />
+      <FAQ />
     </div>
   );
 }

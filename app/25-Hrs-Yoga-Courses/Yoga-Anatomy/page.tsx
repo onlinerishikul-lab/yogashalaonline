173a3a2a8a03a5e9df98363d5452406ee702fe81
santhoshkumar <kumarsanthosh2743@gmail.com>
2025-05-05 14:18:13 +0530

    5-05

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
@@ -2,7 +2,7 @@
 "use client";// app/25-Hrs-Yoga-Courses/page.tsx
 import HeroSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/HeroSection";
 import TestimonialSlider from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialSlider";
-import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/yogacours";
+import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/yogacourse";
 import LearnSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/LearnSection";
 import UniqueCourseSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/UniqueCourseSection";
 import TeachingApproach from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TeachingApproach";

bca63f6e0e9c0643dd2742fc044aaa3450bc4243
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-06 15:45:48 +0530

    Update CertificatePromo.tsx

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CertificatePromo.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CertificatePromo.tsx
@@ -9,12 +9,12 @@ const CertificatePromo = () => {
         {/* Text Content */}
         <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-3xl md:text-5xl font-extrabold mb-5 leading-tight">
-            Excited to See Your <br /> Name on This Certificate?
+            Ready to Earn Your <br /> 25-hrs Yoga Anatomy Certificate?
           </h2>
           <p className="text-lg mb-8 text-[#f9fafb]">
-            Prepare yourself to take the next step and proudly see your name
-            showcased on this certificate as a symbol of your hard work and
-            achievement!
+            Begin your journey into the science of movement and safety in yoga. 
+            Complete our online course and proudly receive your certificate as a 
+            symbol of your growth and dedication.
           </p>
           <Link href="/get-certificate" passHref legacyBehavior>
             <a className="inline-block bg-[#f9fafb] text-[#4377b2] font-semibold px-8 py-3 rounded-full shadow-md hover:bg-white transition duration-300">
@@ -27,7 +27,7 @@ const CertificatePromo = () => {
         <div className="md:w-1/2 flex justify-center">
           <Image
             src="/course-certificate.jpeg"
-            alt="Yoga Certificate Preview"
+            alt="25-hrs Yoga Anatomy Certificate Preview"
             width={512}
             height={212}
             className="w-full max-w-md h-auto rounded-lg shadow-lg"

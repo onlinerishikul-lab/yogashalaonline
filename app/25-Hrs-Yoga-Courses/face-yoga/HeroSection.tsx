e670b1698e3d8ff683898ed9e0703947b253c9c6
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-07 17:50:44 +0530

    Update HeroSection.tsx

--- app/25-Hrs-Yoga-Courses/face-yoga/HeroSection.tsx
+++ app/25-Hrs-Yoga-Courses/face-yoga/HeroSection.tsx
@@ -14,9 +14,9 @@ const HeroSection = () => {
       {/* Hero Content Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-white px-4 pt-20">
         <h1 className="text-4xl md:text-5xl font-bold text-center leading-snug drop-shadow-lg">
-          Yoga Anotomy For Safety
+          Face Yoga TTC 
           <br />
-          Anatomy meets awareness.
+          Master Facial Alignment With Mindful Movement.
         </h1>
         <button className="mt-6 bg-[#4377b2] hover:bg-[#365e8f] transition-colors duration-300 text-white py-3 px-8 rounded-full text-lg font-semibold shadow-lg">
           Enroll Now

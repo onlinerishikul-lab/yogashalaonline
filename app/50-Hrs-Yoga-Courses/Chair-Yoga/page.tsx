d2a75752749174e110bfd46e352f69f95a867ef9
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-09 00:09:50 +0530

    Update page.tsx

--- app/50-Hrs-Yoga-Courses/Chair-Yoga/page.tsx
+++ app/50-Hrs-Yoga-Courses/Chair-Yoga/page.tsx
@@ -1,19 +1,19 @@
 
 "use client";
-import HeroSection from "@/app/25-Hrs-Yoga-Courses/face-yoga/HeroSection";
-import TestimonialSlider from "@/app/25-Hrs-Yoga-Courses/face-yoga/TestimonialSlider";
-import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/face-yoga/yogacourse";
-import LearnSection from "@/app/25-Hrs-Yoga-Courses/face-yoga/LearnSection";
-import UniqueCourseSection from "@/app/25-Hrs-Yoga-Courses/face-yoga/UniqueCourseSection";
-import TeachingApproach from "@/app/25-Hrs-Yoga-Courses/face-yoga/TeachingApproach";
-import CourseDetails from "@/app/25-Hrs-Yoga-Courses/face-yoga/CourseDetails";
-import YogaAccreditation from "@/app/25-Hrs-Yoga-Courses/face-yoga/YogaAccreditation";
-import CertificatePromo from "@/app/25-Hrs-Yoga-Courses/face-yoga/CertificatePromo";
-import FacultyInfo from "@/app/25-Hrs-Yoga-Courses/face-yoga/FacultyInfo";
-import PricingOptions from "@/app/25-Hrs-Yoga-Courses/face-yoga/PricingOptions";
-import RiskFreeGuarantee from "@/app/25-Hrs-Yoga-Courses/face-yoga/RiskFreeGuarantee";
-import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/face-yoga/TestimonialCarousel";
-import Faq from "@/app/25-Hrs-Yoga-Courses/face-yoga/Faq";
+import HeroSection from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/HeroSection";
+import TestimonialSlider from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/TestimonialSlider";
+import YogaCoursePage from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/yogacourse";
+import LearnSection from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/LearnSection";
+import UniqueCourseSection from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/UniqueCourseSection";
+import TeachingApproach from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/TeachingApproach";
+import CourseDetails from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/CourseDetails";
+import YogaAccreditation from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/YogaAccreditation";
+import CertificatePromo from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/CertificatePromo";
+import FacultyInfo from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/FacultyInfo";
+import PricingOptions from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/PricingOptions";
+import RiskFreeGuarantee from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/RiskFreeGuarantee";
+import TestimonialCarousel from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/TestimonialCarousel";
+import Faq from "@/app/50-Hrs-Yoga-Courses/Chair-Yoga/Faq";
 
 export default function Page() {
   return (

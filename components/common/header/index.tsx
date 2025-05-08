b08cb6cfb01cae4d67131412ef7e39bebb931297
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-08 15:22:22 +0530

    Update index.tsx

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -235,7 +235,7 @@ export const Header = () => {
                   <ChevronDown className="w-4 h-4" />
                 </button>
                 {activeDropdown === item.title && (
-                  <div className="absolute bg-white shadow-lg top-full mt-2 rounded-md w-64 z-50 p-2 space-y-1">
+                  <div className="absolute top-0 left-full ml-1 bg-white shadow-lg rounded-md min-w-[300px] z-50 p-2 space-y-1 whitespace-nowrap">>
                     {item.dropdown.map((subItem, idx) => (
                       <div key={subItem.title} className="relative group">
                         {idx > 0 && <div className="border-t border-gray-200 my-1"></div>}
@@ -257,8 +257,9 @@ export const Header = () => {
                           {subItem.title}
                           {subItem.subDropdown && <ChevronRight className="w-4 h-4" />}
                         </button>
-                        {openSubDropdown === subItem.title && subItem.subDropdown && (
-                          <div className="absolute top-0 left-full ml-1 bg-white shadow-lg rounded-md w-64 z-50 p-2 space-y-1">
+                       {openSubDropdown === subItem.title && subItem.subDropdown && (
+                       <div className="absolute top-0 left-full ml-1 bg-white shadow-lg rounded-md w-64 z-50 p-2 space-y-1">
+
                             {subItem.subDropdown.map((nestedItem, nestedIdx) => (
                               <div key={nestedItem.title}>
                                 {nestedIdx > 0 && <div className="border-t border-gray-200 my-1"></div>}

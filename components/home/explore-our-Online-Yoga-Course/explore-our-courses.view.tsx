93d69a5c07f683184a8ad3047186fd55d5e648ba
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-09 16:41:58 +0530

    Update explore-our-courses.view.tsx

--- components/home/explore-our-Online-Yoga-Course/explore-our-courses.view.tsx
+++ components/home/explore-our-Online-Yoga-Course/explore-our-courses.view.tsx
@@ -29,13 +29,13 @@ export default function ExploreOurCoursesView({
   );
 
   return (
-    <section className="overflow-x-hidden bg-[#FBF6F3] py-3">
-      <div className="space-y-5">
+    <section className="overflow-x-hidden bg-[#FBF6F3]">
+      <div className="space-y-0">
         <h2 className="text-4xl font-bold text-center text-[#4377B2]">
           Explore Our Online Yoga  Courses
         </h2>
 
-        <div className={`py-10 bg-[#FBF6F3] ${styles.explore_our_courses}`}>
+        <div className={`py-9 bg-[#FBF6F3] ${styles.explore_our_courses}`}>
           <Swiper
             modules={[Autoplay]}
             spaceBetween={20}

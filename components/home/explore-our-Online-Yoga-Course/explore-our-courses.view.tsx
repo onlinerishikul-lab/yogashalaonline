2a2b63292ef90b9892dd1050bdb0f7b7ba4a7f9d
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-09 16:35:13 +0530

    Update explore-our-courses.view.tsx

--- components/home/explore-our-Online-Yoga-Course/explore-our-courses.view.tsx
+++ components/home/explore-our-Online-Yoga-Course/explore-our-courses.view.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import React from "react";
 import { useRouter } from "next/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -29,16 +30,12 @@ export default function ExploreOurCoursesView({
 
   return (
     <section className="overflow-x-hidden bg-[#FBF6F3] py-3">
-      <div className={`relative ${styles.explore_our_courses}`}>
-        {/* Centered Heading */}
-        <div className="absolute w-full top-[40%] z-10">
-          <h2 className="text-4xl font-bold text-center text-[#4377B2]">
-            Explore Our Online Yoga Courses
-          </h2>
-        </div>
+      <div className="space-y-5">
+        <h2 className="text-4xl font-bold text-center text-[#4377B2]">
+          Explore Our Online Yoga  Courses
+        </h2>
 
-        {/* Swiper Carousel */}
-        <div className="pt-24 pb-10">
+        <div className={`py-10 bg-[#FBF6F3] ${styles.explore_our_courses}`}>
           <Swiper
             modules={[Autoplay]}
             spaceBetween={20}
@@ -85,8 +82,7 @@ export default function ExploreOurCoursesView({
           </Swiper>
         </div>
 
-        {/* Explore More Button */}
-        <div className="text-center mt-4">
+        <div className="text-center">
           <button
             className="bg-[#4377B2] text-white px-8 py-1 rounded-full font-semibold hover:bg-[#285384] transition-colors"
             onClick={() => router.push("/courses")}

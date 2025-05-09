4275f2a5084da8264cf3fb84ef3c89cbd492ab38
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-10 00:59:30 +0530

    Update online-yoga.tsx

--- components/home/online-yoga/online-yoga.tsx
+++ components/home/online-yoga/online-yoga.tsx
@@ -34,93 +34,91 @@ const OnlineYoga = () => {
   };
 
   return (
-    <div>
-      <div className="container mx-auto w-full pb-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 py-4 md:w-full md:justify-center">
-          <div className='h-[200px] bg-[url("/assets/contact-image.png")] bg-cover md:w-full md:h-auto'></div>
-          <div className="bg-[#4377B2] p-2 py-6 md:p-16">
-            <div className='bg-[url("/assets/yogavector.png")] bg-center bg-contain bg-no-repeat'>
-              <form onSubmit={handleSubmit(onSubmit)} className="">
-                <h1 className="uppercase font-bold text-white text-xl">
-                  Want to learn more about our Yoga Teacher Training? Just fill
-                  out the form below, and we&apos;ll provide you with all the
-                  details.
-                </h1>
+    <div className="w-full m-0 p-0">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className='h-[200px] bg-[url("/assets/contact-image.png")] bg-cover w-full md:h-auto'></div>
+        <div className="bg-[#4377B2] p-4 md:p-12 flex items-center">
+          <div className='w-full bg-[url("/assets/yogavector.png")] bg-center bg-contain bg-no-repeat'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <h1 className="uppercase font-bold text-white text-xl">
+                Want to learn more about our Yoga Teacher Training? Just fill
+                out the form below, and we&apos;ll provide you with all the
+                details.
+              </h1>
 
-                <div className="grid grid-cols-1 gap-2 pt-4 md:w-3/4">
-                  <label
-                    htmlFor="name"
-                    className="text-white text-base upercase font-semibold uppercase"
-                  >
-                    Name
-                  </label>
-                  <input
-                    {...register("name")}
-                    type="text"
-                    id="name"
-                    className="outline-none p-1 bg-transparent text-white border-white border-2"
-                  />
-                  {errors.name && (
-                    <span className="text-white text-sm">
-                      {errors.name.message}
-                    </span>
-                  )}
-                </div>
+              <div className="grid grid-cols-1 gap-2 pt-4 md:w-3/4">
+                <label
+                  htmlFor="name"
+                  className="text-white text-base font-semibold uppercase"
+                >
+                  Name
+                </label>
+                <input
+                  {...register("name")}
+                  type="text"
+                  id="name"
+                  className="outline-none p-1 bg-transparent text-white border-white border-2"
+                />
+                {errors.name && (
+                  <span className="text-white text-sm">
+                    {errors.name.message}
+                  </span>
+                )}
+              </div>
 
-                <div className="grid grid-cols-1 gap-2 pt-4 md:w-3/4">
-                  <label
-                    htmlFor="email"
-                    className="text-white text-base upercase font-semibold uppercase"
-                  >
-                    Email
-                  </label>
-                  <input
-                    {...register("email")}
-                    type="email"
-                    id="email"
-                    className="outline-none p-1 bg-transparent text-white border-white border-2"
-                  />
-                  {errors.email && (
-                    <span className="text-white text-sm">
-                      {errors.email.message}
-                    </span>
-                  )}
-                </div>
+              <div className="grid grid-cols-1 gap-2 pt-4 md:w-3/4">
+                <label
+                  htmlFor="email"
+                  className="text-white text-base font-semibold uppercase"
+                >
+                  Email
+                </label>
+                <input
+                  {...register("email")}
+                  type="email"
+                  id="email"
+                  className="outline-none p-1 bg-transparent text-white border-white border-2"
+                />
+                {errors.email && (
+                  <span className="text-white text-sm">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
 
-                <div className="grid grid-cols-1 gap-2 pt-4 md:w-3/4">
-                  <label
-                    htmlFor="phoneNumber"
-                    className="text-white text-base upercase font-semibold uppercase"
-                  >
-                    Phone Number
-                  </label>
-                  <input
-                    {...register("phoneNumber")}
-                    type="tel"
-                    id="phoneNumber"
-                    className="outline-none p-1 bg-transparent text-white border-white border-2"
-                  />
-                  {errors.phoneNumber && (
-                    <span className="text-white text-sm">
-                      {errors.phoneNumber.message}
-                    </span>
-                  )}
-                </div>
+              <div className="grid grid-cols-1 gap-2 pt-4 md:w-3/4">
+                <label
+                  htmlFor="phoneNumber"
+                  className="text-white text-base font-semibold uppercase"
+                >
+                  Phone Number
+                </label>
+                <input
+                  {...register("phoneNumber")}
+                  type="tel"
+                  id="phoneNumber"
+                  className="outline-none p-1 bg-transparent text-white border-white border-2"
+                />
+                {errors.phoneNumber && (
+                  <span className="text-white text-sm">
+                    {errors.phoneNumber.message}
+                  </span>
+                )}
+              </div>
 
-                <div className="grid grid-cols-1 gap-2 pt-4">
-                  <Button
-                    type="submit"
-                    className="bg-white text-[#4377B2] py-3 text-xl font-semibold md:w-min md:px-5 rounded-none"
-                  >
-                    Start now
-                  </Button>
-                  <p className="text-white text-sm mt-2">
-                    * Your privacy is important to us, and we will never share
-                    your information.
-                  </p>
-                </div>
-              </form>
-            </div>
+              <div className="grid grid-cols-1 gap-2 pt-4">
+                <Button
+                  type="submit"
+                  className="bg-white text-[#4377B2] py-3 text-xl font-semibold md:w-min md:px-5 rounded-none"
+                >
+                  Start now
+                </Button>
+                <p className="text-white text-sm mt-2">
+                  * Your privacy is important to us, and we will never share
+                  your information.
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>

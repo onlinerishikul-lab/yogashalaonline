a0590430c122697295fd5a3e1710c3eb2d1b74ce
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-13 12:50:09 +0530

    Update usp.tsx

--- components/home/usp/usp.tsx
+++ components/home/usp/usp.tsx
@@ -36,10 +36,10 @@ const Usp = () => {
             <div className="bg-gradient-to-b from-[#4377B2] to-[#285384] rounded-full h-[80px] w-[80px] sm:h-[90px] sm:w-[90px] lg:h-[100px] lg:w-[100px] p-2 flex items-center justify-center mb-2 sm:mb-3">
               <Image
                 src={item.icon}
-                height={60}
-                width={60}
+                height={30}
+                width={30}
                 alt="usp icon"
-                className="w-[45px] h-[45px] sm:w-[50px] sm:h-[50px] lg:w-[60px] lg:h-[60px]"
+                className="w-[35px] h-[35px] sm:w-[40px] sm:h-[40px] lg:w-[50px] lg:h-[50px]"
               />
             </div>
             <h2 className="text-[#4377B2] text-xl sm:text-2xl lg:text-2xl font-semibold text-center">

812e4598ff15d7795043490634bbf8fca31f5e98
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-13 12:19:28 +0530

    Update guarantee-section.tsx

--- components/home/guarantee-section.tsx
+++ components/home/guarantee-section.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export function GuaranteeSection({ guaranteeBody }: { guaranteeBody: string }) {
   return (
-    <section className="bg-[#4377B2] px-6 py-20 text-white md:px-16 md:py-28">
+    <section className="bg-[#4377B2] px-6  text-white md:px-16">
       <div className="mx-auto max-w-6xl">
         <h2 className="mb-6 text-center text-4xl font-bold leading-tight md:text-4xl">
           Our Guarantee To You: Learn Risk-Free

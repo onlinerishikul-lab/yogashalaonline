e68ef93b38ef56bc7310dd877f39b02c9d27b978
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-14 14:58:12 +0530

    Update about-data.ts

--- constants/about-data.ts
+++ constants/about-data.ts
@@ -3,7 +3,7 @@ export const teachers: { id: number; name: string; image: string; description: s
     id: 1,
     name: "Gangesha Chaitanya",
     image: "/Gangesha-Chaitanya.jpg",
-    description: `Gangesha Chaitanya (Yoga Philosophy, Meditation, Pranayama And Spiritual Guidance)
+    description: `Gangesha Chaitanya<br/><small>(Yoga Philosophy, Meditation, Pranayama And Spiritual Guidance)</small><br/><br/>
 Explore the captivating world of Yogi Gangesha Chaitanya, a master of Yoga Philosophy, Meditation, Pranayama, and Spiritual Guidance. Guided by the revered saint Swami Rama from a young age, Yogi Gangesha has become a beacon of wisdom and compassion.
 
 Immerse yourself in the transformative teachings of Yogi Gangesha Chaitanya, a master of Yoga Philosophy, Meditation, Pranayama, and Spiritual Guidance. With guidance from revered saints and immersion in renowned yoga traditions, Yogi Gangesha offers a harmonious blend of ancient wisdom and modern insights. Experience the serene aura of his classes as he leads you on a journey to awaken your kundalini energy and explore higher consciousness. Join him to unlock the limitless potential of your mind and spirit, embracing the magic of meditation and spiritual wisdom. Let Yogi Gangesha illuminate your path to inner harmony and enlightenment.`
@@ -12,7 +12,7 @@ Immerse yourself in the transformative teachings of Yogi Gangesha Chaitanya, a m
     id: 2,
     name: "Sukra Chiatanya",
     image: "/Sukra Chiatanya.png",
-    description: `Sukra Chiatanya (Meditation, Mantra And Pranayama)
+    description: `Sukra Chiatanya<br/><small>(Meditation, Mantra And Pranayama)</small><br/><br/>
 Welcome to the serene world of Yogi Sukra Chaitanya, hailing from the charming villages of West Odisha, India. He immersed himself in the study of human nature and the environment, mastering educational psychology and environmental sciences at Sambalpur University. Seeking deeper wisdom, he ventured into the spiritual realm, diving into Yoga Sciences and Indian Philosophy at Dev Sanskriti Haridwar University.
 
 Under the guidance of renowned masters like Swami Veda Bharati from the Himalayan Tradition, Yogi Sukra Chaitanya embarked on a journey to greater heights. He further honed his skills in traditional Kriya yoga under the wise mentorship of Swami Sankaranand Giri and Swami Nityanandaji of the Yoga Satsang Society, India.
@@ -23,7 +23,7 @@ Yogi Sukra Chaitanya's teachings resonate like a beautiful symphony, bringing ha
     id: 3,
     name: "Purnima Ji",
     image: "/Purnima Ji.png",
-    description: `Purnima Ji (Pranayama & Meditation)
+    description: `Purnima Ji<br/><small>(Pranayama & Meditation)</small><br/><br/>
 Embark on a transformative journey with Purnima, whose spiritual quest began in the sacred lands of Karnataka, steeped in the teachings of Basavanna. Through her deep-rooted spiritual upbringing, Purnima found herself drawn to the path of inner awakening at an early age. Guided by revered mentors like Sri Ganeswer Avadhut and Swami Veda Bharthi, she delved into the profound practices of pranayama, meditation, and philosophy, enriching her spiritual journey.
 
 Driven by a desire to share her wisdom, Purnima devoted herself to serving in ashrams, touching the lives of countless individuals across 42 villages in Karnataka. Her innate ability to forge deep connections resonated deeply with all those she encountered. Upon her arrival in Rishikesh in 2015, Purnima's passion for yoga soared to new heights as she immersed herself in the teachings of skilled yogis, igniting a divine spark within her soul.`
@@ -32,80 +32,28 @@ Driven by a desire to share her wisdom, Purnima devoted herself to serving in as
     id: 4,
     name: "Himanshu Ji",
     image: "/Himanshu Ji.png",
-    description: "John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga."
+    description: `Himanshu Ji<br/><small>(Hatha & Vinyasa Yoga)</small><br/><br/>
+John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga.`
   },
   {
     id: 5,
     name: "Akshay Nautiyal",
     image: "/Akshay Nautiyal.jpg",
-    description: "John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga."
+    description: `Akshay Nautiyal<br/><small>(Hatha & Vinyasa Yoga)</small><br/><br/>
+John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga.`
   },
   {
     id: 6,
     name: "Balram Baloni",
     image: "/Balram Baloni.jpg",
-    description: "John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga."
+    description: `Balram Baloni<br/><small>(Hatha & Vinyasa Yoga)</small><br/><br/>
+John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga.`
   },
   {
     id: 7,
     name: "Swami Amarna ji",
     image: "/Swami Amarna ji.png",
-    description: "John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga."
+    description: `Swami Amarna ji<br/><small>(Hatha & Vinyasa Yoga)</small><br/><br/>
+John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga.`
   }
 ];
-
-
-export const reviews = [
-  {
-    id: 1,
-    text: "Your app brings so much peace and tolerance to our home.",
-    author: "Rachael",
-    location: "UK",
-    context: "on meditation's positive effect on family life"
-  },
-  {
-    id: 2,
-    text: "I came to learn that the storyline in my head was holding me back.",
-    author: "Peter",
-    location: "Belgium",
-    context: "on what he learned when sitting with himself"
-  },
-  {
-    id: 3,
-    text: "Headspace provides me with a connection to myself, and a disconnection from negative thoughts, feelings, and sensations.",
-    author: "Keri",
-    location: "UK",
-    context: "on finding her happy place"
-  },
-  {
-    id: 4,
-    text: "Changing my thoughts has allowed me to change my life.",
-    author: "Davide",
-    location: "London",
-    context: "on using meditation to turn his life around"
-  }
-]
-
-export const courses = [
-  {
-    title: "300-HOUR Multi-style Yoga Teacher Training Course - 65 Day Program",
-    image: "https://images.unsplash.com/photo-1602594748821-6df031e275e1?q=80&w=3027&auto=format&fit=crop&ixlib=rb-4.0.3",
-    href: "/courses/kundalini"
-  },
-  {
-    title: "200-HOUR Hatha Yoga Teacher Training Course - 45 Day Program",
-    image: "https://images.unsplash.com/photo-1547852356-b20668106c51?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3",
-    href: "/courses/pranayama"
-  },
-  {
-    title: "100-Vinyasa-Flow-Yoga-Teacher-Training Course - 30 Days Program",
-    image: "https://plus.unsplash.com/premium_photo-1666264200751-8a013663a89b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3",
-    href: "/courses/meditation"
-  },
-  {
-    title: "50-Hour Online Yin Yoga Teacher Training Course – 15 Days Program",
-    image: "https://plus.unsplash.com/premium_photo-1666264200751-8a013663a89b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3",
-    href: "/courses/meditation"
-  }
-]
-

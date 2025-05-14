8f432831f2bd62686daf8df6097d8b9a86c80378
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-14 15:02:14 +0530

    Update about-data.ts

--- constants/about-data.ts
+++ constants/about-data.ts
@@ -57,3 +57,56 @@ John is a certified yoga instructor with 10+ years of experience in Hatha and Vi
 John is a certified yoga instructor with 10+ years of experience in Hatha and Vinyasa yoga.`
   }
 ];
+export const reviews = [
+  {
+    id: 1,
+    text: "Your app brings so much peace and tolerance to our home.",
+    author: "Rachael",
+    location: "UK",
+    context: "on meditation's positive effect on family life"
+  },
+  {
+    id: 2,
+    text: "I came to learn that the storyline in my head was holding me back.",
+    author: "Peter",
+    location: "Belgium",
+    context: "on what he learned when sitting with himself"
+  },
+  {
+    id: 3,
+    text: "Headspace provides me with a connection to myself, and a disconnection from negative thoughts, feelings, and sensations.",
+    author: "Keri",
+    location: "UK",
+    context: "on finding her happy place"
+  },
+  {
+    id: 4,
+    text: "Changing my thoughts has allowed me to change my life.",
+    author: "Davide",
+    location: "London",
+    context: "on using meditation to turn his life around"
+  }
+]
+
+export const courses = [
+  {
+    title: "300-HOUR Multi-style Yoga Teacher Training Course - 65 Day Program",
+    image: "https://images.unsplash.com/photo-1602594748821-6df031e275e1?q=80&w=3027&auto=format&fit=crop&ixlib=rb-4.0.3",
+    href: "/courses/kundalini"
+  },
+  {
+    title: "200-HOUR Hatha Yoga Teacher Training Course - 45 Day Program",
+    image: "https://images.unsplash.com/photo-1547852356-b20668106c51?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3",
+    href: "/courses/pranayama"
+  },
+  {
+    title: "100-Vinyasa-Flow-Yoga-Teacher-Training Course - 30 Days Program",
+    image: "https://plus.unsplash.com/premium_photo-1666264200751-8a013663a89b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3",
+    href: "/courses/meditation"
+  },
+  {
+    title: "50-Hour Online Yin Yoga Teacher Training Course – 15 Days Program",
+    image: "https://plus.unsplash.com/premium_photo-1666264200751-8a013663a89b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3",
+    href: "/courses/meditation"
+  }
+]

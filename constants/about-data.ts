ba59df062fe052ea17265d64a3963c401c8e5729
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-14 15:24:39 +0530

    Update about-data.ts

--- constants/about-data.ts
+++ constants/about-data.ts
@@ -3,16 +3,16 @@ export const teachers: { id: number; name: string; image: string; description: s
     id: 1,
     name: "Gangesha Chaitanya",
     image: "/Gangesha-Chaitanya.jpg",
-    description: `Gangesha Chaitanya (Yoga Philosophy, Meditation, Pranayama And Spiritual Guidance)
-Explore the captivating world of Yogi Gangesha Chaitanya, a master of Yoga Philosophy, Meditation, Pranayama, and Spiritual Guidance. Guided by the revered saint Swami Rama from a young age, Yogi Gangesha has become a beacon of wisdom and compassion.
-
+    Subtitle: "(Yoga Philosophy, Meditation, Pranayama And Spiritual Guidance)",
+    description: `Explore the captivating world of Yogi Gangesha Chaitanya, a master of Yoga Philosophy, Meditation, Pranayama, and Spiritual Guidance. Guided by the revered saint Swami Rama from a young age, Yogi Gangesha has become a beacon of wisdom and compassion.
 Immerse yourself in the transformative teachings of Yogi Gangesha Chaitanya, a master of Yoga Philosophy, Meditation, Pranayama, and Spiritual Guidance. With guidance from revered saints and immersion in renowned yoga traditions, Yogi Gangesha offers a harmonious blend of ancient wisdom and modern insights. Experience the serene aura of his classes as he leads you on a journey to awaken your kundalini energy and explore higher consciousness. Join him to unlock the limitless potential of your mind and spirit, embracing the magic of meditation and spiritual wisdom. Let Yogi Gangesha illuminate your path to inner harmony and enlightenment.`
   },
   {
     id: 2,
     name: "Sukra Chiatanya",
     image: "/Sukra Chiatanya.png",
-    description: `Sukra Chiatanya (Meditation, Mantra And Pranayama)
+    Subtitle:"(Meditation, Mantra And Pranayama)",
+    description: `
 Welcome to the serene world of Yogi Sukra Chaitanya, hailing from the charming villages of West Odisha, India. He immersed himself in the study of human nature and the environment, mastering educational psychology and environmental sciences at Sambalpur University. Seeking deeper wisdom, he ventured into the spiritual realm, diving into Yoga Sciences and Indian Philosophy at Dev Sanskriti Haridwar University.
 
 Under the guidance of renowned masters like Swami Veda Bharati from the Himalayan Tradition, Yogi Sukra Chaitanya embarked on a journey to greater heights. He further honed his skills in traditional Kriya yoga under the wise mentorship of Swami Sankaranand Giri and Swami Nityanandaji of the Yoga Satsang Society, India.
@@ -23,9 +23,8 @@ Yogi Sukra Chaitanya's teachings resonate like a beautiful symphony, bringing ha
     id: 3,
     name: "Purnima Ji",
     image: "/Purnima Ji.png",
-    description: `Purnima Ji (Pranayama & Meditation)
-Embark on a transformative journey with Purnima, whose spiritual quest began in the sacred lands of Karnataka, steeped in the teachings of Basavanna. Through her deep-rooted spiritual upbringing, Purnima found herself drawn to the path of inner awakening at an early age. Guided by revered mentors like Sri Ganeswer Avadhut and Swami Veda Bharthi, she delved into the profound practices of pranayama, meditation, and philosophy, enriching her spiritual journey.
-
+    Subtitle:"(Pranayama & Meditation)",
+    description: `Embark on a transformative journey with Purnima, whose spiritual quest began in the sacred lands of Karnataka, steeped in the teachings of Basavanna. Through her deep-rooted spiritual upbringing, Purnima found herself drawn to the path of inner awakening at an early age. Guided by revered mentors like Sri Ganeswer Avadhut and Swami Veda Bharthi, she delved into the profound practices of pranayama, meditation, and philosophy, enriching her spiritual journey.
 Driven by a desire to share her wisdom, Purnima devoted herself to serving in ashrams, touching the lives of countless individuals across 42 villages in Karnataka. Her innate ability to forge deep connections resonated deeply with all those she encountered. Upon her arrival in Rishikesh in 2015, Purnima's passion for yoga soared to new heights as she immersed herself in the teachings of skilled yogis, igniting a divine spark within her soul.`
   },
   {

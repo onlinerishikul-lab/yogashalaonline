a577f002191c881cfff84d1570abc0af3bc41948
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-15 21:35:50 +0530

    Update page.tsx

--- app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
+++ app/25-Hrs-Yoga-Courses/Yoga-Anatomy/page.tsx
@@ -14,7 +14,6 @@ import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Testimon
 import Faq from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
-
 export default function Page() {
   return (
     <div>

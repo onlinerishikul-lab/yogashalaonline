af535c9e0676a2d943d5a014339764b5ef87a69f
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-16 12:15:44 +0530

    Update page.tsx

--- app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/page.tsx
+++ app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/page.tsx
@@ -9,11 +9,10 @@ import TeachingApproach from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/Teac
 import CourseDetails from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/CourseDetails";
 import YogaAccreditation from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/YogaAccreditation";
 import CertificatePromo from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/CertificatePromo";
-import FacultyInfo from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/FacultyInfo";
-import PricingOptions from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/PricingOptions";
-import RiskFreeGuarantee from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/RiskFreeGuarantee";
 import TestimonialCarousel from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/TestimonialCarousel";
 import Faq from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/Faq";
+import { SiteFooter } from "@/components/common/footer";
+import Faculty from "@/components/home/Faculty/faculty";
 
 export default function Page() {
   return (
@@ -27,11 +26,10 @@ export default function Page() {
       <CourseDetails />
       <YogaAccreditation/>
       <CertificatePromo/>
-      <FacultyInfo/>
-      <PricingOptions/>
-      <RiskFreeGuarantee/>
+      <Faculty/>
       <TestimonialCarousel/>
       <Faq/>
+      <SiteFooter/>
     </div>
   );
 }

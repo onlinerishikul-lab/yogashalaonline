b5935b760b625138a6d0811588c9d03c86034dba
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-16 14:30:07 +0530

    Update page.tsx

--- app/Online-Yoga-Classes/Hatha-Yoga/page.tsx
+++ app/Online-Yoga-Classes/Hatha-Yoga/page.tsx
@@ -9,11 +9,10 @@ import TeachingApproach from "@/app/Online-Yoga-Classes/Hatha-Yoga/TeachingAppro
 import CourseDetails from "@/app/Online-Yoga-Classes/Hatha-Yoga/CourseDetails";
 import YogaAccreditation from "@/app/Online-Yoga-Classes/Hatha-Yoga/YogaAccreditation";
 import CertificatePromo from "@/app/Online-Yoga-Classes/Hatha-Yoga/CertificatePromo";
-import FacultyInfo from "@/app/Online-Yoga-Classes/Hatha-Yoga/FacultyInfo";
-import PricingOptions from "@/app/Online-Yoga-Classes/Hatha-Yoga/PricingOptions";
-import RiskFreeGuarantee from "@/app/Online-Yoga-Classes/Hatha-Yoga/RiskFreeGuarantee";
 import TestimonialCarousel from "@/app/Online-Yoga-Classes/Hatha-Yoga/TestimonialCarousel";
 import Faq from "@/app/Online-Yoga-Classes/Hatha-Yoga/Faq";
+import { SiteFooter } from "@/components/common/footer";
+import Faculty from "@/components/home/Faculty/faculty";
 
 export default function Page() {
   return (
@@ -27,11 +26,10 @@ export default function Page() {
       <CourseDetails />
       <YogaAccreditation/>
       <CertificatePromo/>
-      <FacultyInfo/>
-      <PricingOptions/>
-      <RiskFreeGuarantee/>
+      <Faculty/>
       <TestimonialCarousel/>
       <Faq/>
+      <SiteFooter />
     </div>
   );
 }

2dd033be4da7664c6d2cb95d902e5cf08fafa586
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-19 14:11:35 +0530

    Update experiences.tsx

--- components/home/experiences/experiences.tsx
+++ components/home/experiences/experiences.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import TestimonialSlider from './experiences-slider'
+import React from "react";
+import TestimonialSlider from "./experiences-slider";
 
 const Testimonial = () => {
   return (
-    <TestimonialSlider/>
-  )
-}
+    <section className="py-16 md:py-24 bg-[#FCF6F3]">
+      {/* Optional: Add a heading */}
+      {/* <h2 className="text-center text-2xl md:text-4xl font-bold text-[#4377B2] mb-12">Testimonials</h2> */}
+      <TestimonialSlider />
+    </section>
+  );
+};
 
-export default Testimonial
+export default Testimonial;

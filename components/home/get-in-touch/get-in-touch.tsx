fe62e1d6acc88970a2b4ded0696a69bc1b58ecc5
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-20 18:49:00 +0530

    Update get-in-touch.tsx

--- components/home/get-in-touch/get-in-touch.tsx
+++ components/home/get-in-touch/get-in-touch.tsx
@@ -138,14 +138,7 @@ const GetInTouch = () => {
 
         {/* Right Section - Map */}
         <div className="w-full md:w-1/2 h-[300px] md:h-auto">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2622052.696491503!2d9.844387287499998!3d50.0750835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b937cc088de5f%3A0xab1a6c291f681d4!2sOffice%20option%2C%20Ltd.!5e0!3m2!1sen!2sin!4v1737019150873!5m2!1sen!2sin"
-            className="w-full h-full rounded-lg"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497700.1123005915!2d77.30127814132629!3d12.95379021332858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1747747101265!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>

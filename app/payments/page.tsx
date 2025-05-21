f00fa025c76375a46d9badbd7a9ce9f0f74842d8
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-21 16:00:18 +0530

    Update page.tsx

--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Header } from "@/components/common/header";
 import { SiteFooter } from "@/components/common/footer";
-import PaymentPage from "@/components/payments/PaymentPage";
+import PaymentPage from "@/app/payments/PaymentPage";
 
 export default function Page() {
   return (

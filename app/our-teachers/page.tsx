76bfb921bd824bba168048da092ba66b301eb403
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-21 18:05:25 +0530

    Update page.tsx

--- app/our-teachers/page.tsx
+++ app/our-teachers/page.tsx
@@ -1,35 +1,11 @@
 
 "use client";
-import HeroSection from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/HeroSection";
-import TestimonialSlider from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/TestimonialSlider";
-import YogaCoursePage from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/yogacourse";
-import LearnSection from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/LearnSection";
-import UniqueCourseSection from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/UniqueCourseSection";
-import TeachingApproach from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/TeachingApproach";
-import CourseDetails from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/CourseDetails";
-import YogaAccreditation from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/YogaAccreditation";
-import CertificatePromo from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/CertificatePromo";
-import TestimonialCarousel from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/TestimonialCarousel";
-import Faq from "@/app/300-Hrs-Yoga-Courses/Multi-Style-Yoga-TTC/Faq";
-import { SiteFooter } from "@/components/common/footer";
-import Faculty from "@/components/home/Faculty/faculty";
+import TeachersPage from "@/app/our-teachers/teacher"
 
 export default function Page() {
   return (
     <div>
-      <HeroSection />
-      <TestimonialSlider />
-      <YogaCoursePage />
-      < LearnSection/>
-      <UniqueCourseSection />
-      <TeachingApproach />
-      <CourseDetails />
-      <YogaAccreditation/>
-      <CertificatePromo/>
-      <Faculty/>
-      <TestimonialCarousel/>
-      <Faq/>
-      <SiteFooter />
+      <TeachersPage />
     </div>
   );
 }

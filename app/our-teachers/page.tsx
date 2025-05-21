3f9d8ac8e7afb4da1705b0a95e1dbc0f0c6b5f72
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-21 18:10:03 +0530

    Update page.tsx

--- app/our-teachers/page.tsx
+++ app/our-teachers/page.tsx
@@ -1,11 +1,15 @@
 
 "use client";
-import TeachersPage from "@/app/our-teachers/teacher"
+import TeachersPage from "@/app/our-teachers/teachers"
+import { Header } from "@/components/common/header";
+import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {
   return (
     <div>
+      <Header />
       <TeachersPage />
+      <SiteFooter />
     </div>
   );
 }

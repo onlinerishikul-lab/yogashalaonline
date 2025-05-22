fc953bb792fcac979318020931af9b04624ab0f5
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-22 10:49:24 +0530

    Update banner.tsx

--- components/home/Banner/banner.tsx
+++ components/home/Banner/banner.tsx
@@ -54,7 +54,7 @@ export function Banner() {
           height={70}
         />
         <Image
-          src="/rpys5.png"
+          src="/rpys4.png"
           alt="RYS 300 Yoga Alliance"
           width={70}
           height={70}

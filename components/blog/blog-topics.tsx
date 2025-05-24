d6e64617097c5129c558129752cfb24528ae4ba7
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-24 12:37:46 +0530

    Update blog-topics.tsx

--- components/blog/blog-topics.tsx
+++ components/blog/blog-topics.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useMemo } from "react";
+import { useMemo } from "react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { BlogCard } from "./blog-card";

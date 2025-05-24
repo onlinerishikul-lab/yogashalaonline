574ec8a8b2b152052e34176ce4f598b0f11b640f
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-24 14:54:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,18 +9,6 @@ const nextConfig: NextConfig = {
     deviceSizes: [320, 420, 768, 1024, 1200, 1600],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  experimental: {
-    modularizeImports: {
-      // Example: Optimizing lodash imports
-      lodash: {
-        transform: "lodash/{{member}}",
-      },
-      // You can add more libraries here
-      "date-fns": {
-        transform: "date-fns/{{member}}",
-      },
-    },
-  },
   compiler: {
     // Optional: remove console.logs from production
     removeConsole: process.env.NODE_ENV === "production",

51ff999a6a29658f6f2258cfa034fed06e689413
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-26 17:16:15 +0530

    Update blog-topics.tsx

--- components/blog/blog-topics.tsx
+++ components/blog/blog-topics.tsx
@@ -55,7 +55,7 @@ export function BlogTopics({ posts }: BlogTopicsProps) {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredPosts.map((post) => (
-          <BlogCard key={post.id} post={post} className="h-full" />
+          <BlogCard key={post.id} post={post} />
         ))}
       </div>
     </section>

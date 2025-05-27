69990d5da531996d2b13a563dd1b79c305a0ad07
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-27 20:38:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Yogshala",
-  description: "Yogshala",
+  title: "Rishikul ",
+  description: "Rishikul ",
 };
 
 export default function RootLayout({

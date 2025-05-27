abb5ce8e851e61e853048a96dd72c66de8895e0a
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-27 14:01:44 +0530

    Update explore-our-courses.view.tsx

--- components/home/explore-our-Online-Yoga-Classes/explore-our-courses.view.tsx
+++ components/home/explore-our-Online-Yoga-Classes/explore-our-courses.view.tsx
@@ -31,7 +31,7 @@ export default function ExploreOurClassesView({
     <section className="overflow-x-hidden bg-[#FBF6F3] py-3">
       <div className={`py-7 ${styles.explore_our_courses}`}>
         <h2 className="text-4xl font-bold text-center text-[#4377B2] mb-6 z-10 relative">
-          Explore Our Online Yoga Courses
+          Explore Our Online Yoga Classes
         </h2>
 
         <Swiper

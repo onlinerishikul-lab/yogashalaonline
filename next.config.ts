5b20070ead58e62c527a4abc3e29219ff4a98df6
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-28 14:55:02 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,37 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
   images: {
     formats: ["image/avif", "image/webp"],
     domains: ["images.unsplash.com", "plus.unsplash.com", "utfs.io"],
-    deviceSizes: [360, 414, 768, 1024, 1280, 1440, 1920],
+    deviceSizes: [320, 420, 768, 1024, 1200, 1600],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-
   compiler: {
+    // Optional: remove console.logs from production
     removeConsole: process.env.NODE_ENV === "production",
-
-    modularizeImports: {
-      "react-icons": {
-        transform: "react-icons/{{member}}",
-      },
-      lodash: {
-        transform: "lodash/{{member}}",
-      },
-      "date-fns": {
-        transform: "date-fns/{{member}}",
-      },
-    },
   },
-
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-    serverActions: true,
-  },
-
-  staticPageGenerationTimeout: 60,
 };
 
 export default nextConfig;

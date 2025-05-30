2ade6d997d1fdb17c7e93be83b2977aa9acb79a2
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-30 11:38:32 +0530

    Update Guarante.tsx

--- components/home/Guarantee/Guarante.tsx
+++ components/home/Guarantee/Guarante.tsx
@@ -10,10 +10,10 @@ const Guarante = () => {
     <div className="bg-[#4377B2] bg-cover w-full py-5 sm:py-3">
       <div className="max-w-5xl mx-auto text-white text-center px-4 sm:px-6">
         <h2 className="mb-6 text-2xl sm:text-3xl md:text-4xl font-bold leading-tight">
-          Ready to begin your Online Yoga Teacher Training journey from the comfort of your home?
+          What is Rishikul Online Yoga Teacher Training?
         </h2>
         <p className="mb-10 text-sm sm:text-base md:text-lg leading-relaxed text-white/90">
-          Rishikul Online Yoga Teacher Training program offers a transformative experience with live interactive sessions and an internationally certified curriculum. Learn at your own pace, connect with experienced instructors, and receive personalized guidance every step of the way. Begin your journey to becoming a certified yoga teacher today!
+         A certified online program with live classes, expert instructors, and flexible learning from home.
         </p>
         <div className="flex justify-center">
           <Button
@@ -21,7 +21,7 @@ const Guarante = () => {
             onClick={() => router.push("/courses")}
             className="bg-white text-[#4377B2] hover:text-white hover:bg-[#365e92] hover:border-white border border-transparent font-semibold w-full sm:w-[240px] py-4 rounded-full transition-all duration-300"
           >
-            JOIN TODAY!
+            Explore More
           </Button>
         </div>
       </div>

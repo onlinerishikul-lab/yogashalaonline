fb66d3355e2ac11e413c3101b231bcd4f1ae437d
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-05-31 16:17:31 +0530

    Update page.tsx

--- app/50-Hrs-Yoga-Courses/Restorative-Yoga-TTC/page.tsx
+++ app/50-Hrs-Yoga-Courses/Restorative-Yoga-TTC/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import HeroSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/HeroSection";
-import StickyCourseCard from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialSlider";
-import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/yogacourse";
-import LearnSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/LearnSection";
-import CourseDetails from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseDetails";
-import YogaAccreditation from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/YogaAccreditation";
-import CertificatePromo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CertificatePromo";
-import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialCarousel";
-import Faq from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Faq";
+import HeroSection from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/HeroSection";
+import StickyCourseCard from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/TestimonialSlider";
+import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/yogacourse";
+import LearnSection from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/LearnSection";
+import CourseDetails from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/CourseDetails";
+import YogaAccreditation from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/YogaAccreditation";
+import CertificatePromo from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/CertificatePromo";
+import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/TestimonialCarousel";
+import Faq from "@/app/25-Hrs-Yoga-Courses/Restorative-Yoga-TTC/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

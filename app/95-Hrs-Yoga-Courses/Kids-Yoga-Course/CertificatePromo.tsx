058a13d231c7092c4f767b42e405cc082c6604e3
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-01 13:53:41 +0530

    Update CertificatePromo.tsx

--- app/95-Hrs-Yoga-Courses/Kids-Yoga-Course/CertificatePromo.tsx
+++ app/95-Hrs-Yoga-Courses/Kids-Yoga-Course/CertificatePromo.tsx
@@ -9,10 +9,9 @@ const CertificatePromo = () => {
         {/* Text Content */}
         <div className="w-full md:w-1/2 text-center md:text-left">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold mb-5 leading-tight">
-            Earn Your Yoga Anatomy Certificate
-          </h2>
+            Earn Your Kids Yoga Teaching Certificate          </h2>
           <p className="text-base sm:text-lg mb-8 text-[#f9fafb]">
-            Begin your journey into yoga anatomy and safe practice. Complete our online 25-hour course and receive a certificate that reflects your growth and commitment.
+            Step into a heart-centered role as a kids yoga teacher. Complete our 95-hour training and receive your certificate — a symbol of your commitment to nurturing the next generation with joy, balance, and mindfulness.
           </p>
           <Link href="/contact">
             <a className="inline-block bg-[#f9fafb] text-[#4377b2] font-semibold px-6 sm:px-8 py-2.5 sm:py-3 rounded-full shadow-md hover:bg-white transition duration-300 text-sm sm:text-base">

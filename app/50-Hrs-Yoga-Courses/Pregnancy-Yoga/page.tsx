34e4031de9f28b9af003f4361c654928b9814e8a
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-01 12:07:28 +0530

    Update page.tsx

--- app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/page.tsx
+++ app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import HeroSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/HeroSection";
-import StickyCourseCard from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialSlider";
-import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/yogacourse";
-import LearnSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/LearnSection";
-import CourseDetails from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseDetails";
-import YogaAccreditation from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/YogaAccreditation";
-import CertificatePromo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CertificatePromo";
-import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialCarousel";
-import Faq from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Faq";
+import HeroSection from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/HeroSection";
+import StickyCourseCard from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/TestimonialSlider";
+import YogaCoursePage from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/yogacourse";
+import LearnSection from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/LearnSection";
+import CourseDetails from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/CourseDetails";
+import YogaAccreditation from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/YogaAccreditation";
+import CertificatePromo from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/CertificatePromo";
+import TestimonialCarousel from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/TestimonialCarousel";
+import Faq from "@/app/50-Hrs-Yoga-Courses/Pregnancy-Yoga/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

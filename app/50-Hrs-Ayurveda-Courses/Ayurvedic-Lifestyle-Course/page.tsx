5b1aca9cc3a5680f1a14e0dce3a9ca58920d2cc8
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-02 17:12:13 +0530

    Update page.tsx

--- app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/page.tsx
+++ app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import HeroSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/HeroSection";
-import StickyCourseCard from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialSlider";
-import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/yogacourse";
-import LearnSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/LearnSection";
-import CourseDetails from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseDetails";
-import YogaAccreditation from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/YogaAccreditation";
-import CertificatePromo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CertificatePromo";
-import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialCarousel";
-import Faq from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Faq";
+import HeroSection from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/HeroSection";
+import StickyCourseCard from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/TestimonialSlider";
+import YogaCoursePage from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/yogacourse";
+import LearnSection from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/LearnSection";
+import CourseDetails from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/CourseDetails";
+import YogaAccreditation from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/YogaAccreditation";
+import CertificatePromo from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/CertificatePromo";
+import TestimonialCarousel from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/TestimonialCarousel";
+import Faq from "@/app/50-Hrs-Ayurveda-Courses/Ayurvedic-Lifestyle-Course/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

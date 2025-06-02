1ac5a538f4ce35ec472f04662ab04cd4c5cbf909
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-02 14:50:28 +0530

    Update CertificatePromo.tsx

--- app/25-Hrs-Ayurveda-Courses/Ayurveda-Skin-and-Beauty-Course/CertificatePromo.tsx
+++ app/25-Hrs-Ayurveda-Courses/Ayurveda-Skin-and-Beauty-Course/CertificatePromo.tsx
@@ -9,10 +9,10 @@ const CertificatePromo = () => {
         {/* Text Content */}
         <div className="w-full md:w-1/2 text-center md:text-left">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold mb-5 leading-tight">
-            Earn Your Yoga Anatomy Certificate
+            Earn Your Ayurveda Skin & Beauty Certificate
           </h2>
           <p className="text-base sm:text-lg mb-8 text-[#f9fafb]">
-            Begin your journey into yoga anatomy and safe practice. Complete our online 25-hour course and receive a certificate that reflects your growth and commitment.
+           Complete the course and receive a recognized certification that reflects your understanding of Ayurvedic beauty care and holistic skin health.
           </p>
           <Link href="/contact">
             <a className="inline-block bg-[#f9fafb] text-[#4377b2] font-semibold px-6 sm:px-8 py-2.5 sm:py-3 rounded-full shadow-md hover:bg-white transition duration-300 text-sm sm:text-base">

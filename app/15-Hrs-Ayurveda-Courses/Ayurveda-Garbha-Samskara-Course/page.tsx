7b88372c812949917423b9deca099d251eb07ae1
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-02 12:58:24 +0530

    Update page.tsx

--- app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/page.tsx
+++ app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import HeroSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/HeroSection";
-import StickyCourseCard from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialSlider";
-import YogaCoursePage from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/yogacourse";
-import LearnSection from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/LearnSection";
-import CourseDetails from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CourseDetails";
-import YogaAccreditation from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/YogaAccreditation";
-import CertificatePromo from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/CertificatePromo";
-import TestimonialCarousel from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/TestimonialCarousel";
-import Faq from "@/app/25-Hrs-Yoga-Courses/Yoga-Anatomy/Faq";
+import HeroSection from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/HeroSection";
+import StickyCourseCard from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/TestimonialSlider";
+import YogaCoursePage from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/yogacourse";
+import LearnSection from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/LearnSection";
+import CourseDetails from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/CourseDetails";
+import YogaAccreditation from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/YogaAccreditation";
+import CertificatePromo from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/CertificatePromo";
+import TestimonialCarousel from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/TestimonialCarousel";
+import Faq from "@/app/15-Hrs-Ayurveda-Courses/Ayurveda-Garbha-Samskara-Course/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

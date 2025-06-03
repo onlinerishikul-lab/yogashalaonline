aed375994da7aea860d5407689d70e15effe77fa
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-03 12:33:19 +0530

    Update page.tsx

--- app/Online-Yoga-Classes/Prenatal-Yoga/page.tsx
+++ app/Online-Yoga-Classes/Prenatal-Yoga/page.tsx
@@ -1,34 +1,49 @@
 
 "use client";
 import HeroSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/HeroSection";
-import TestimonialSlider from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialSlider";
+import StickyCourseCard  from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialSlider";
 import YogaCoursePage from "@/app/Online-Yoga-Classes/Prenatal-Yoga/yogacourse";
 import LearnSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/LearnSection";
-import UniqueCourseSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/UniqueCourseSection";
-import TeachingApproach from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TeachingApproach";
 import CourseDetails from "@/app/Online-Yoga-Classes/Prenatal-Yoga/CourseDetails";
 import YogaAccreditation from "@/app/Online-Yoga-Classes/Prenatal-Yoga/YogaAccreditation";
 import CertificatePromo from "@/app/Online-Yoga-Classes/Prenatal-Yoga/CertificatePromo";
 import TestimonialCarousel from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialCarousel";
 import Faq from "@/app/Online-Yoga-Classes/Prenatal-Yoga/Faq";
 import { SiteFooter } from "@/components/common/footer";
-import Faculty from "@/components/home/Faculty/faculty";
 
 export default function Page() {
   return (
-    <div>
+    <div className="flex flex-col gap-10">
+      {/* Full-width Hero Section */}
       <HeroSection />
-      <TestimonialSlider />
-      <YogaCoursePage />
-      < LearnSection/>
-      <UniqueCourseSection />
-      <TeachingApproach />
-      <CourseDetails />
-      <YogaAccreditation/>
-      <CertificatePromo/>
-      <Faculty/>
-      <TestimonialCarousel/>
-      <Faq/>
+
+      {/* Main content with sticky card on desktop */}
+      <div className="flex flex-col lg:flex-row gap-10 px-4 md:px-8 xl:px-20">
+        {/* Left content */}
+        <div className="w-full lg:w-[75%] space-y-10">
+          <YogaCoursePage />
+          <LearnSection />
+          <CourseDetails />
+          <YogaAccreditation />
+          <CertificatePromo />
+          <TestimonialCarousel />
+          <Faq />
+        </div>
+
+        {/* Right sticky card */}
+        <div className="hidden lg:block w-full lg:w-[25%]">
+          <div className="sticky top-24">
+            <StickyCourseCard />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile view: sticky card appears below all content */}
+      <div className="block lg:hidden px-4 md:px-8 xl:px-20">
+        <StickyCourseCard />
+      </div>
+
+      {/* Full-width Footer */}
       <SiteFooter />
     </div>
   );

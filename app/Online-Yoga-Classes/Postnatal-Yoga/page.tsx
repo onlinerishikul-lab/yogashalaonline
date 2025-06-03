0dca102bcb8df61f5bbd02cefdbab6c6db65ff72
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-03 13:15:57 +0530

    Update page.tsx

--- app/Online-Yoga-Classes/Postnatal-Yoga/page.tsx
+++ app/Online-Yoga-Classes/Postnatal-Yoga/page.tsx
@@ -1,14 +1,13 @@
-
 "use client";
-import HeroSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/HeroSection";
-import StickyCourseCard  from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialSlider";
-import YogaCoursePage from "@/app/Online-Yoga-Classes/Prenatal-Yoga/yogacourse";
-import LearnSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/LearnSection";
-import CourseDetails from "@/app/Online-Yoga-Classes/Prenatal-Yoga/CourseDetails";
-import YogaAccreditation from "@/app/Online-Yoga-Classes/Prenatal-Yoga/YogaAccreditation";
-import CertificatePromo from "@/app/Online-Yoga-Classes/Prenatal-Yoga/CertificatePromo";
-import TestimonialCarousel from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialCarousel";
-import Faq from "@/app/Online-Yoga-Classes/Prenatal-Yoga/Faq";
+import HeroSection from "@/app/Online-Yoga-Classes/Postnatal-Yoga/HeroSection";
+import StickyCourseCard  from "@/app/Online-Yoga-Classes/Postnatal-Yoga/TestimonialSlider";
+import YogaCoursePage from "@/app/Online-Yoga-Classes/Postnatal-Yoga/yogacourse";
+import LearnSection from "@/app/Online-Yoga-Classes/Postnatal-Yoga/LearnSection";
+import CourseDetails from "@/app/Online-Yoga-Classes/Postnatal-Yoga/CourseDetails";
+import YogaAccreditation from "@/app/Online-Yoga-Classes/Postnatal-Yoga/YogaAccreditation";
+import CertificatePromo from "@/app/Online-Yoga-Classes/Postnatal-Yoga/CertificatePromo";
+import TestimonialCarousel from "@/app/Online-Yoga-Classes/Postnatal-Yoga/TestimonialCarousel";
+import Faq from "@/app/Online-Yoga-Classes/Postnatal-Yoga/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

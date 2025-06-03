3d224e0b2a0cff7f1004f5be9f44a9485ae468cb
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-03 13:17:02 +0530

    Update CertificatePromo.tsx

--- app/Online-Yoga-Classes/Postnatal-Yoga/CertificatePromo.tsx
+++ app/Online-Yoga-Classes/Postnatal-Yoga/CertificatePromo.tsx
@@ -9,11 +9,10 @@ const CertificatePromo = () => {
         {/* Text Content */}
         <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-3xl md:text-5xl font-extrabold mb-5 leading-tight">
-            Restore and Renew with <br /> Postnatal Yoga
+            Earn Your Postnatal Yoga Certificate
           </h2>
           <p className="text-lg mb-8 text-[#f9fafb]">
-            Support your recovery journey with our specialized Postnatal Yoga course, 
-            designed to gently rebuild strength and promote well-being after childbirth.
+            Support new mothers with confidence and compassion. Complete our Postnatal Yoga Course and receive your certificate as a mark of your knowledge, care, and teaching readiness.
           </p>
           <Link href="/postnatal-yoga" passHref legacyBehavior>
             <a className="inline-block bg-[#f9fafb] text-[#4377b2] font-semibold px-8 py-3 rounded-full shadow-md hover:bg-white transition duration-300">

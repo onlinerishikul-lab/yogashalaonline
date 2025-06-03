8ae5b5a6170e770ba1bd2d1c842fe26ef79b9ef8
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-03 10:39:42 +0530

    Update Guarante.tsx

--- components/home/Guarantee/Guarante.tsx
+++ components/home/Guarantee/Guarante.tsx
@@ -1,13 +1,14 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { useRouter } from "next/navigation";
 
-const Guarante = () => {
+const GuaranteeSection = () => {
   const router = useRouter();
 
   return (
-    <div className="bg-[#4377B2] bg-cover w-full py-5 sm:py-3">
+    <section className="bg-[#4377B2] bg-cover w-full py-5 sm:py-3">
       <div className="max-w-5xl mx-auto text-white text-center px-4 sm:px-6">
         <h2 className="mb-6 text-2xl sm:text-3xl md:text-4xl font-bold leading-tight">
           What is Rishikul Online Yoga Teacher Training?
@@ -27,8 +28,8 @@ const Guarante = () => {
           </Button>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default Guarante;
+export default GuaranteeSection;

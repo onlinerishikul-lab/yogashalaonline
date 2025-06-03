7c55bc0583ba4d7a74963ed5ce0970083e87caa8
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-03 15:02:31 +0530

    Update page.tsx

--- app/Online-Yoga-Classes/Vinyasa-Flow/page.tsx
+++ app/Online-Yoga-Classes/Vinyasa-Flow/page.tsx
@@ -1,14 +1,14 @@
 
 "use client";
-import HeroSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/HeroSection";
-import StickyCourseCard  from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialSlider";
-import YogaCoursePage from "@/app/Online-Yoga-Classes/Prenatal-Yoga/yogacourse";
-import LearnSection from "@/app/Online-Yoga-Classes/Prenatal-Yoga/LearnSection";
-import CourseDetails from "@/app/Online-Yoga-Classes/Prenatal-Yoga/CourseDetails";
-import YogaAccreditation from "@/app/Online-Yoga-Classes/Prenatal-Yoga/YogaAccreditation";
-import CertificatePromo from "@/app/Online-Yoga-Classes/Prenatal-Yoga/CertificatePromo";
-import TestimonialCarousel from "@/app/Online-Yoga-Classes/Prenatal-Yoga/TestimonialCarousel";
-import Faq from "@/app/Online-Yoga-Classes/Prenatal-Yoga/Faq";
+import HeroSection from "@/app/Online-Yoga-Classes/Vinyasa-Flow/HeroSection";
+import StickyCourseCard  from "@/app/Online-Yoga-Classes/Vinyasa-Flow/TestimonialSlider";
+import YogaCoursePage from "@/app/Online-Yoga-Classes/Vinyasa-Flow/yogacourse";
+import LearnSection from "@/app/Online-Yoga-Classes/Vinyasa-Flow/LearnSection";
+import CourseDetails from "@/app/Online-Yoga-Classes/Vinyasa-Flow/CourseDetails";
+import YogaAccreditation from "@/app/Online-Yoga-Classes/Vinyasa-Flow/YogaAccreditation";
+import CertificatePromo from "@/app/Online-Yoga-Classes/Vinyasa-Flow/CertificatePromo";
+import TestimonialCarousel from "@/app/Online-Yoga-Classes/Vinyasa-Flow/TestimonialCarousel";
+import Faq from "@/app/Online-Yoga-Classes/Vinyasa-Flow/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

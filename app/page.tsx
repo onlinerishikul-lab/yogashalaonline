be9455e2a53b2080d5e0418ac014fdf729fb4ee8
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-08 22:10:54 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,45 @@
+import { Header } from '@/components/common/header'
+import { SiteFooter } from '@/components/common/footer'
 import Blogs from '@/components/home/blogs/blogs'
-import CoreView from '@/components/home/core-values/core.view'
 import Faculty from '@/components/home/Faculty/faculty'
 import GetInTouch from '@/components/home/get-in-touch/get-in-touch'
 import Join from '@/components/home/join/join'
-import OnlineYoga from '@/components/home/online-yoga/online-yoga'
 import OurTeaching from '@/components/home/our-teaching/our-teaching'
-import Testimonial from '@/components/home/experiences/experiences'
 import Transforming from '@/components/home/transforming/transforming'
 import Trust from '@/components/home/trust/trust'
 import GuaranteeSection from '@/components/home/Guarantee/Guarante'
 import Usp from '@/components/home/usp/usp'
-import React from 'react'
-import TestimonialView from '@/components/home/testimonial/testimonial'
-import MainWrapper from '@/components/wrappers/main-wrapper'
+import Testimonial from '@/components/home/testimonial/testimonial'
 import { Banner } from '@/components/home/Banner/banner'
-import ExploreOurCoursesView from '@/components/home/explore-our-courses/explore-our-courses.view'
+import ExploreOurCoursesView from '@/components/home/explore-our-Online-Yoga-Course/explore-our-courses.view'
+import ExploreOurAyurvedaCoursesView from '@/components/home/explore-our-Online-Ayurveda-Course/explore-our-courses.view'
+import ExploreOurClassesView from '@/components/home/explore-our-Online-Yoga-Classes/explore-our-courses.view'
+
 import { getCourses } from '@/server-actions/course'
 import { getBlogs } from '@/server-actions/blog'
+
+import BenefitsOfOnlineLearning from '@/components/home/core-values/benefits-of-online-learning'
+import CourseLevels from '@/components/home/core-values/course-levels'
+import YogaForEveryone from '@/components/home/core-values/yoga-for-everyone'
 
 export default async function Home() {
   const [{ courses = [], error: coursesError }, { blogs = [], error: blogsError }] = await Promise.all([
     getCourses(),
     getBlogs()
-  ]);
+  ])
 
   if (coursesError || blogsError) {
-    return <div className="text-red-500 text-center py-8">{coursesError || blogsError}</div>;
+    return <div className="text-red-500 text-center py-8">{coursesError || blogsError}</div>
   }
 
   return (
-    <MainWrapper>
-      <Banner />
-      <Usp />
-      {/* Section 1: Benefits + Courses */}
+    <div>
+      <Header />
+      {/* This wrapper ensures content is not hidden behind the fixed header */}
+        <Banner />
+        <Usp/> {/* ✅ Fix: Add required 'title' prop */}
+        
+        {/* Section 1: Benefits + Courses */}
         <section className="bg-white py-0">
           <div className="container mx-auto px-2">
             <ExploreOurCoursesView courses={courses} />
@@ -57,17 +64,17 @@ export default async function Home() {
             <YogaForEveryone />
           </div>
         </section>
-      <GuaranteeSection />
-      <Trust />
-      <OurTeaching />
-      <Transforming courses={courses} />
-      <OnlineYoga />
-      <Blogs blogs={blogs} />
-      <Join />
-      <Faculty />
-      <Testimonial />
-      <TestimonialView title="What Our Students Say" tilt={true}/>
-      <GetInTouch />
-    </MainWrapper>
+
+        <GuaranteeSection />
+        <OurTeaching />
+        <Transforming />
+        <Trust />
+        <Blogs blogs={blogs} />
+        <Join />
+        <Faculty />
+        <Testimonial title="What Our Students Say" tilt={true}/>
+        <GetInTouch />
+      <SiteFooter />
+    </div>
   )
 }

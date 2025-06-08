4e6a9f383715c4e2cb2070059f00d5ef3ef31bd9
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-08 22:27:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "standalone", // ✅ enables standalone build for deployment
   images: {
-    formats: ["image/avif", "image/webp"],
-    domains: [
-      "images.unsplash.com",
-      "plus.unsplash.com",
-      "utfs.io",
-      "rishikulonlineadmin.vercel.app"
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "utfs.io",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "rishikulonlineadmin.vercel.app",
-        pathname: "/**",
-      }
-    ],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+    domains: ["images.unsplash.com", "plus.unsplash.com", "utfs.io"],
   },
 };
 

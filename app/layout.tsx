6b514ed7b17cf723c274ba14b1a211e87245e59b
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-08 09:54:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="min-h-full">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f9f9f9] text-[#171717] min-h-screen m-0 p-0`}
       >
         <QueryProvider>{children}</QueryProvider>
       </body>

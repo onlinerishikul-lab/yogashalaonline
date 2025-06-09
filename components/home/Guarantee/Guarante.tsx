f159959d66776d185a63de11eb52d7eed8fde7ac
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-09 11:28:10 +0530

    Update Guarante.tsx

--- components/home/Guarantee/Guarante.tsx
+++ components/home/Guarantee/Guarante.tsx
@@ -11,12 +11,10 @@ const GuaranteeSection = () => {
     <section className="bg-[#4377B2] bg-cover w-full py-5 sm:py-3">
       <div className="max-w-5xl mx-auto text-white text-center px-4 sm:px-6">
         <h2 className="mb-6 text-2xl sm:text-3xl md:text-4xl font-bold leading-tight">
-          What is Rishikul Online Yoga Teacher Training?
+          Why Rishikul Online is Best for Yoga?
         </h2>
         <p className="mb-10 text-base sm:text-lg md:text-xl leading-relaxed text-white/90 max-w-3xl mx-auto">
-          A certified online program with live classes, expert instructors,
-          <br className="hidden sm:block" />
-          and flexible learning from home.
+         Rishikul Online offers authentic, expert-led yoga training from the comfort of your home with flexible, globally certified courses.
         </p>
         <div className="flex justify-center">
           <Button

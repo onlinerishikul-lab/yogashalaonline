96d45dcb092b7c8cad6c055d37242e37d51efdd3
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-09 13:04:43 +0530

    Update CourseDetails.tsx

--- app/Online-Yoga-Classes/Prenatal-Yoga/CourseDetails.tsx
+++ app/Online-Yoga-Classes/Prenatal-Yoga/CourseDetails.tsx
@@ -83,10 +83,10 @@ const CourseDetails = () => {
     <section className="bg-[#f9fafb] text-[#4377b2] py-16 px-4 sm:px-6">
       <div className="max-w-6xl mx-auto text-center">
         <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6">
-          Benefits of the Prenatal Yoga Course
+          Benefits of the Prenatal Yoga Class
         </h2>
         <p className="mb-10 max-w-2xl mx-auto text-base sm:text-lg leading-relaxed text-[#4377b2]/90">
-          This course empowers expecting mothers with physical, mental, and emotional tools to navigate pregnancy with ease, mindfulness, and strength.
+          This Class empowers expecting mothers with physical, mental, and emotional tools to navigate pregnancy with ease, mindfulness, and strength.
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 text-left">
           {features.map((feature) => (

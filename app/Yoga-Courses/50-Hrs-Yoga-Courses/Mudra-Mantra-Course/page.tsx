209c25872f2864a97df68159cf70778b06eb7a4c
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-16 13:05:03 +0530

    Update page.tsx

--- app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/page.tsx
+++ app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import HeroSection from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/HeroSection";
-import StickyCourseCard from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/TestimonialSlider";
-import YogaCoursePage from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/yogacourse";
-import LearnSection from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/LearnSection";
-import CourseDetails from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/CourseDetails";
-import YogaAccreditation from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/YogaAccreditation";
-import CertificatePromo from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/CertificatePromo";
-import TestimonialCarousel from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/TestimonialCarousel";
-import Faq from "@/app/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/Faq";
+import HeroSection from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/HeroSection";
+import StickyCourseCard from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/TestimonialSlider";
+import YogaCoursePage from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/yogacourse";
+import LearnSection from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/LearnSection";
+import CourseDetails from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/CourseDetails";
+import YogaAccreditation from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/YogaAccreditation";
+import CertificatePromo from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/CertificatePromo";
+import TestimonialCarousel from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/TestimonialCarousel";
+import Faq from "@/app/Yoga-Courses/50-Hrs-Yoga-Courses/Mudra-Mantra-Course/Faq";
 import { SiteFooter } from "@/components/common/footer";
 
 export default function Page() {

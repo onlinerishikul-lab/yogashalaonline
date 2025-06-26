36041de4dbf96c5586e6563050700ccb7a2d2601
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-26 15:49:05 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,6 +10,7 @@ export default function AboutPage() {
     <MainWrapper>
       <div className="bg-white">
         <div className="h-screen min-h-fit md:h-screen md:min-h-[800px]">
+          {/* Hero Image */}
           <div className="relative h-1/2 w-full">
             <Image
               src="https://images.unsplash.com/photo-1528319725582-ddc096101511?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3"
@@ -21,12 +22,12 @@ export default function AboutPage() {
           </div>
 
           {/* Content Section */}
-          <div className="container mx-auto py-16">
-            <h1 className="text-[#4377B2] text-4xl font-bold mb-8">
-              About Rishikul Yogshala
+          <div className="container mx-auto py-3 px-4">
+            <h1 className="text-[#4377B2] text-4xl font-bold mb-6 text-center md:text-left">
+              About Rishikul Online
             </h1>
 
-            <div className="space-y-6 text-gray-700 max-w-7xl">
+            <div className="space-y-6 text-gray-700 max-w-7xl mx-auto">
               <p className="leading-relaxed">
                 Welcome to Rishikul Online, your one-stop destination for all
                 yoga teacher training (YTTC Online) courses. Founded in 2010,
@@ -37,7 +38,7 @@ export default function AboutPage() {
                 Our team of experienced and certified yoga instructors have
                 curated a curriculum that covers everything from traditional
                 Hatha and Ashtanga yoga to modern variations like aerial and
-                prenatal yoga. With interactive live webinars, and personalized
+                prenatal yoga. With interactive live webinars and personalized
                 feedback sessions, we ensure that you receive a top-notch
                 education from the comfort of your own home.
               </p>
@@ -46,10 +47,11 @@ export default function AboutPage() {
                 not just about mastering physical postures but also
                 understanding the philosophy and spirituality behind this
                 5000-year-old practice. That&apos;s why our courses also include
-                modules on meditation, Ayurveda, and yogic philosophy.Join us on
-                this journey of self-discovery and transformation as you become
-                a certified yoga teacher with Rishikul Online. Let us guide you
-                towards your true potential in mind, body, and soul. Namaste.
+                modules on meditation, Ayurveda, and yogic philosophy. Join us
+                on this journey of self-discovery and transformation as you
+                become a certified yoga teacher with Rishikul Online. Let us
+                guide you towards your true potential in mind, body, and soul.
+                Namaste.
               </p>
             </div>
           </div>

a53adcdb2b593ac28d68e4342e6a515c43c22b1d
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-06-27 12:39:43 +0530

    Update yoga-for-everyone.tsx

--- components/home/core-values/yoga-for-everyone.tsx
+++ components/home/core-values/yoga-for-everyone.tsx
@@ -3,7 +3,7 @@ import { CoreValuesSection } from "@/components/core-values";
 
 const YogaForEveryone = () => {
   const audience = [
-    { icon: "/assets/core-values/kids.svg", text: "Kids" },
+    { icon: "/assets/core-values/1.png", text: "Kids" },
     { icon: "/assets/core-values/woman.svg", text: "Women" },
     { icon: "/assets/core-values/man.svg", text: "Men" },
   ];

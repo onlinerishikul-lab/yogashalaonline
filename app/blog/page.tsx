4d3169a4689a465c3fa30076a63da68ff2b4e272
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-08-07 10:24:56 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useMemo, useEffect } from 'react'
+import { useState, useMemo } from 'react'
 import dynamic from 'next/dynamic'
 import MainWrapper from '@/components/wrappers/main-wrapper'
 import { Skeleton } from '@/components/ui/skeleton'

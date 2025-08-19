b78986efb2e32968c3dc8db2c2abe2bef895fb7a
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-08-19 12:38:59 +0530

    Update benefits-of-online-learning.tsx

--- components/home/core-values/benefits-of-online-learning.tsx
+++ components/home/core-values/benefits-of-online-learning.tsx
@@ -3,9 +3,9 @@ import { CoreValuesSection } from "@/components/core-values";
 
 const BenefitsOfOnlineLearning = () => {
   const benefits = [
-    { icon: "/assets/3.png", text: "Affordable" },
-    { icon: "/assets/7.png", text: "Can Join From Anywhere" },
-    { icon: "/assets/5.png", text: "Global Access" },
+    { icon: "/assets/3.png", text: "Affordable", size: 150 },
+    { icon: "/assets/7.png", text: "Can Join From Anywhere", size: 150 },
+    { icon: "/assets/5.png", text: "Global Access", size: 150 },
   ];
 
   return (

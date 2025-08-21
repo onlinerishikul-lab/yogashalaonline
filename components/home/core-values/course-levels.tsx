a05d0f30c9ce2bfd50bad1550b17103cf2d59d12
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-08-21 17:14:33 +0530

    Update course-levels.tsx

--- components/home/core-values/course-levels.tsx
+++ components/home/core-values/course-levels.tsx
@@ -3,16 +3,13 @@ import { CoreValuesSection } from "@/components/core-values";
 
 const CourseLevels = () => {
   const levels = [
-    { icon: "/assets/4.png", text: "Beginner Course" },
-    { icon: "/assets/8.png", text: "Intermediate Course" },
-    { icon: "/assets/2.png", text: "Advance Course" },
+    { icon: "/assets/4.png", text: "Beginner Course", size: 100 },
+    { icon: "/assets/8.png", text: "Intermediate Course", size: 100 },
+    { icon: "/assets/2.png", text: "Advance Course", size: 100 },
   ];
 
   return (
-    <CoreValuesSection
-      title="Course Levels"
-      values={levels}
-    />
+    <CoreValuesSection title="Course Levels" values={levels} />
   );
 };
 

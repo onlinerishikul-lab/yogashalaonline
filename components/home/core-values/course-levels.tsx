127944364488c41b0de5d5d06ecf4214a24f8559
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2025-08-21 17:39:40 +0530

    Update course-levels.tsx

--- components/home/core-values/course-levels.tsx
+++ components/home/core-values/course-levels.tsx
@@ -3,9 +3,9 @@ import { CoreValuesSection } from "@/components/core-values";
 
 const CourseLevels = () => {
   const levels = [
-    { icon: "/assets/4.png", text: "Beginner Course", size: 100 },
-    { icon: "/assets/8.png", text: "Intermediate Course", size: 100 },
-    { icon: "/assets/2.png", text: "Advance Course", size: 100 },
+    { icon: "/assets/4.png", text: "Beginner Course", size: 70 },
+    { icon: "/assets/8.png", text: "Intermediate Course", size: 70 },
+    { icon: "/assets/2.png", text: "Advance Course", size: 70 },
   ];
 
   return (
